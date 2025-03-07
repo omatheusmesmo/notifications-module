d444f684f6bf09ac2d84d959bc6742c8afa4eef1
Matheus Oliveira da Silva <matheus.6148@gmail.com>
2025-03-07 15:02:57 -0300

    feat: update main method to run NotificationService

--- src/main/java/com/notification/Main.java
+++ src/main/java/com/notification/Main.java
@@ -2,6 +2,6 @@ package com.notification;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        new NotificationService().run();
     }
 }
